717deda752585f15017736ce38b1cee1c7612684
Andrew Hart <Andrew@DentReality.com>
2023-05-21 12:13:04 +0100

    adds fresh xcode project

---
+++ Mirador/MiradorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiradorApp.swift
+//  Mirador
+//
+//  Created by Andrew Hart on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiradorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
