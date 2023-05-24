86577d691d0423f9e2927cab68ef3c45bc0e20d2
Andrew Hart <AndrewHartAR@users.noreply.github.com>
2023-05-24 20:40:11 +0100

    adds location entity and location anchor entity, as foundations for AR location support

---
+++ Mirador/Mirador/LocationEntity.swift
@@ -0,0 +1,39 @@
+//
+//  LocationEntity.swift
+//  Mirador
+//
+//  Created by Andrew Hart on 22/05/2023.
+//
+
+import Foundation
+import RealityKit
+
+class LocationEntity: Entity {
+    var location: Location
+    
+    init(location: Location) {
+        self.location = location
+        
+        super.init()
+    }
+    
+    @MainActor required init() {
+        fatalError("init() has not been implemented")
+    }
+}
+
+class LocationAnchorEntity: Entity {
+    let locationAnchor: LocationAnchor
+    var referenceImageName: String?
+    
+    init(locationAnchor: LocationAnchor, referenceImageName: String? = nil) {
+        self.locationAnchor = locationAnchor
+        self.referenceImageName = referenceImageName
+        
+        super.init()
+    }
+    
+    @MainActor required init() {
+        fatalError("init() has not been implemented")
+    }
+}
